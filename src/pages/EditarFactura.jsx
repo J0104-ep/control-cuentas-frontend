b86bfbbe31fd86c5a0f84c8550d963b3a58daa3a
J0104-ep <julianapinedae@gmail.com>
2026-08-03 16:19:29 -0500

    Fix nombre de archivo CSS para Vercel

--- src/pages/EditarFactura.jsx
+++ src/pages/EditarFactura.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import api from "../api/axios";
-import "../styles/nuevafactura.css";
+import "../styles/nuevaFactura.css";
 
 
 function EditarFactura() {
