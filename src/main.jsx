69e7bfe8110e055a31d25fb00a54d6f48c0763e3
J0104-ep <julianapinedae@gmail.com>
2026-08-03 16:35:51 -0500

    Convertir Control Cuentas en PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
+import App from "./App.jsx";
 
-import App from "./App";
+import { registerSW } from "virtual:pwa-register";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+registerSW();
+
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+)
+.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
