6b0bf7b1a89c289a5e3cb0ea278ca3dcbd001d55
J0104-ep <julianapinedae@gmail.com>
2026-08-04 14:20:26 -0500

    Corrige nombre de estilos usuarios

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import api from "../api/axios";
-import "../styles/usuarios.css";
+import "../styles/Usuarios.css";
 
 
 function Usuarios() {
